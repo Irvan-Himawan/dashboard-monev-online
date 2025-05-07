2d89fa89f91d0f9be623b85bc5baaaff6701e000
Aruvisa <himawan.alvisa@gmail.com>
2025-05-07 16:13:01 +0700

    edit dashboard streamlit

--- app.py
+++ app.py
@@ -2,11 +2,19 @@ import streamlit as st
 import pandas as pd
 from data_loader import load_data
 
-st.set_page_config(page_title="Dashboard dari Google Form", layout="wide")
-st.title("📊 Dashboard Data Google Form (Cols A–CG)")
+st.set_page_config(page_title="Dashboard Pengolahan Data Monev BPVP Bandung Barat", layout="wide")
+st.subheader("All data from respondents")
 
+# create refresh button for clear the cache
+if st.button("Refresh Data"):
+    st.cache_data.clear()
+
+# call the data loader function
+@st.cache_data
+def get_data():
+    return load_data()
 # load data
-df = load_data()
+df = get_data()
 st.dataframe(df)
 
 # contoh chart
--- data_loader.py
+++ data_loader.py
@@ -20,4 +20,51 @@ def load_data():
     data_rows = [r for r in raw[1:] if r != header]
 
     df = pd.DataFrame(data_rows, columns=header)
+
+    # cleaning dataset
+
+    # Grouping questions columns
+    columns_materi_pelatihan = [
+    "1. Tulisan di dalam materi pelatihan jelas dan mudah di baca",
+    "2. Kualitas materi pelatihan dapat menambah tingkat keterampilan dan pengetahuan anda",
+    "3. Materi pelatihan mudah di pahami dan mudah diterapkan dalam praktek",
+    "4. Materi pelatihan telah sesuai dengan harapan anda"
+    ]
+
+    columns_materi_penyelenggaraan =  [
+        "1. Pelayanan administrasi pelatihan diberikan dengan baik, cepat tanggap dan jelas",
+        "2. Pelaksanaan pelatihan dimonitor dengan baik",
+        "3. Keluhan peserta pelatihan direspon dengan cepat dan positif",
+        "4. Platform pelatihan yang mudah diakses",
+        "5. Pembelajaran online selama pelatihan dianggan berkualitas baik"
+    ]
+
+    columns_materi_tenaga_pelatih = [
+        "1. Menguasai materi pelatihan teori dan praktek",
+        "2. Menyajikan pelajaran dengan jelas dan bahasanya mudah dimengerti",
+        "3. Memberikan materi sesuai dengan tujuan pembelajaran secara sistematis/berurutan",
+        "4. Memberikan kesempatan pada peserta pelatihan untuk bertanya atau menyampaikan pendapat",
+        "5. Menciptakan suasana belajar yang  kondusif (aman dan nyaman)",
+        "6. Hadir tepat waktu sesuai jadwal"
+    ]
+
+    combine_questions_columns = (
+        columns_materi_pelatihan +
+        columns_materi_penyelenggaraan +
+        columns_materi_tenaga_pelatih
+    )
+
+    # Filtering data by unique email and program name based on the latest timestamp
+    df = df[df['Email Address'].notna() & (df['Email Address'] != ' ')]
+    df = df.sort_values(by=['Nama Program pelatihan yang anda ikuti','Timestamp'],ascending=False)
+    df = df.reset_index(drop=True)
+    df = df.drop_duplicates(subset=['Email Address','Nama Program pelatihan yang anda ikuti'], keep='first')
+
+    # convert questions columns to numeric
+    df[combine_questions_columns] = df[combine_questions_columns].apply(pd.to_numeric, errors='coerce').astype('Int64')
+
+    # split in column nama program pelatihan into 2 columns (batch and program name)
+    df[['Batch', 'Program Pelatihan']] = df['Nama Program pelatihan yang anda ikuti'].str.extract(r'(Batch \d+)\s*-\s*(.+)', expand=True)
+    df['Batch'] = df['Batch'].str.strip()
+    df['Program Pelatihan'] = df['Program Pelatihan'].str.strip()
     return df
